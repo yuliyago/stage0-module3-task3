8254218fef4ec8c7a24308fd4f560c6ab214bae3
yuliyago <yuuliya02@proton.me>
2023-06-20 21:32:18 +0300

    Variables were declared/initialized/reassigned.

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first=1;
+        int second=10;
+        int third=100;
+        System.out.println(first+"\n"+second+"\n"+third);
     }
 }
